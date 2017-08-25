b5b0a5ec9f5ff6b11d19d8f8194e9b57d106838a
vdrg <vrdragicevic@gmail.com>
2017-08-25 11:34:14 -0300

    Basic prototype.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,4 +2,29 @@ pragma solidity ^0.4.13;
 
 contract Splitter {
 
+  mapping (address => uint) public balances;
+  mapping (address => address[]) public splitters;
+
+  function setSpliters(address[] _splitters) {
+    require(_splitters.length > 0);
+    splitters[msg.sender] = _splitters;
+  }
+
+  function deposit() payable {
+    uint splittersLength = splitters[msg.sender].length; 
+
+    require(msg.value > 0 && splittersLength > 0);
+
+    for (uint i = 0; i < splittersLength; i++) {
+      // TODO: figure out what to do with the remaining ether.
+      balances[splitters[msg.sender][i]] += msg.value / splittersLength;
+    }
+  }
+
+  function withdraw() {
+    uint balance = balances[msg.sender];
+    require(balance > 0);
+
+    msg.sender.transfer(balance);
+  }
 }
